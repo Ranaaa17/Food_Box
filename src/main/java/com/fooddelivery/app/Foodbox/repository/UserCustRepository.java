3047dc597b7815f25cef81b4d4102b7b7ffb4d8e
Ranaaa17 <eruguranadhher@gmail.com>
2025-05-13 00:16:03 +0530

    first commit

---
+++ src/main/java/com/fooddelivery/app/Foodbox/repository/UserCustRepository.java
@@ -0,0 +1,9 @@
+package com.fooddelivery.app.Foodbox.repository;
+
+
+import com.fooddelivery.app.Foodbox.model.UserCust;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCustRepository extends JpaRepository<UserCust, Long> {
+	UserCust findByUsername(String username);
+}
